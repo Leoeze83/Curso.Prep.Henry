11ab85162740a714eedd1b742e6079403b850a3e
Leonardo <69331303+Leoeze83@users.noreply.github.com>
2021-02-03 22:12:50 -0300

    11112

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -46,6 +46,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+     return array;
 }
 
 
@@ -55,6 +57,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ");
 }
 
 
