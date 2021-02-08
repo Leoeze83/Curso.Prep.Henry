ab9362de527f9457873c67834f73eebaa073777f
Leonardo <69331303+Leoeze83@users.noreply.github.com>
2021-02-08 18:00:35 -0300

    ....

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -157,7 +157,7 @@ if( newArray[0] === "9"){
  function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí 
+  //Escribe tu código aquí         // VER ....
 
 }
 
