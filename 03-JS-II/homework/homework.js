dd741b809756b0d761e188e57b214e0e08b6e4d1
Leonardo <69331303+Leoeze83@users.noreply.github.com>
2021-02-09 09:21:39 -0300

    1111

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,7 +137,7 @@ function operadoresLogicos(num1, num2, num3) {
   if (num1 > 0 && num1 > num2 && num1 > num3)
     return "Número 1 es mayor y positivo";
   if (num3 > num1 && num3 > num2) return num3 + 1;
-  if (num1 == 0 || num2 == 0 || num3 == 0) return "Error"; //ver
+  if (num1 == 0 || num2 == 0 || num3 == 0) return "Error"; //no sale
   return false;
 }
 
