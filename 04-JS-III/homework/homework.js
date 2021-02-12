b021732da05c6dc4a10da8718d7bf4e77994ffdb
Leonardo <69331303+Leoeze83@users.noreply.github.com>
2021-02-11 22:11:59 -0300

    pasooooo!!!

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -154,6 +154,14 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
+
+  for (var i = 0; i < arreglo.length - 1; i++) {
+    if (arreglo[i] === arreglo[i + 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function mesesDelAño(array) {
@@ -178,16 +186,21 @@ function mesesDelAño(array) {
         break;
     }
   }
-  if (newArray.includes("Enero" && "Marzo" && "Noviembre")) {
+  if (newArray.length < 3) {
+    return "No se encontraron los meses pedidos";
+  } else {
     return newArray;
-  } else return "No se encontraron los meses pedidos";
+  }
 }
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  var newArray = array.filter((array) => array > 100);
+  var newArray = array.filter(function (array) {
+    // var newArray = array.filter((array) => array > 100); // es lo mismo que la linea 199
+    return array > 100;
+  });
   return newArray;
 }
 
